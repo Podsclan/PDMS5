572ff0e2e85c9c9cdd1ed12dc916eb00ddbefd35
Brendow Podsclan <b.podsclan@gmail.com>
2020-11-10 19:05:48 -0300

    implemented new buttons and added concat function

--- app/src/main/java/br/com/brendow/helloworld/MainActivity.java
+++ app/src/main/java/br/com/brendow/helloworld/MainActivity.java
@@ -5,10 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView visorTv;
+    private String visor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v) {
         Button b = (Button)v;
         String buttonText = b.getText().toString();
-        visorTv.setText(buttonText.equals("limpar") ? "" : buttonText);
+        if (buttonText.equals("limpar (C)")) {
+            visor = "";
+        } else {
+            System.out.println(buttonText);
+            visor = visor.concat(buttonText);
+        }
+
+        visorTv.setText(visor);
     }
 }
